8a58b98170e9445fa16ca19e0fd1fc8d1dacaedd
Russell Schick <rschick@bellwoods.ca>
2017-09-26 13:46:45 -0400

    support for removal

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ class BasePathMappingPlugin {
 		this.options = options;
 
 		this.hooks = {
-			"after:deploy:deploy": this._beforeDeploy.bind(this)
+			"after:deploy:deploy": this._afterDeploy.bind(this),
+			"before:remove:remove": this._beforeRemove.bind(this)
 		};
 	}
 
-	_beforeDeploy() {
+	_afterDeploy() {
 		const createBasePathMappingService = new CreateBasePathMappingService(this.serverless, this.options);
 		return createBasePathMappingService.createBasePathMapping();
+	}
+
+	_beforeRemove() {
+		const createBasePathMappingService = new CreateBasePathMappingService(this.serverless, this.options);
+		return createBasePathMappingService.deleteBasePathMapping();
 	}
 }
 
